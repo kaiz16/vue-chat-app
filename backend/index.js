a5d7705086cedb44bbc9a6b4c6d5fb59c28dfab2
mohammadsultani <mohammadssultani44@gmail.com>
2020-10-06 11:27:09 +0800

    Built for deployment

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,12 @@ mongoDb.on('open', () => {
 mongoDb.on('error', (error) => {
     console.log(`Faced Error ${error}`)
 })
-
+// Serves files from our dist directory 
+app.use(express.static(__dirname + '/frontend/dist'))
+// For managing the routes in a SPA
+app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + '/frontend/dist/index.html')
+})
 
 const port = process.env.PORT || 5000
 // Running app on a given port
