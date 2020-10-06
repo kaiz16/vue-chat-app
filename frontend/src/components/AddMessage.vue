c59e98270c415ff04db336206a352746309e60d6
mohammadsultani <mohammadssultani44@gmail.com>
2020-10-06 16:16:47 +0800

    Deploying Again

--- frontend/src/components/AddMessage.vue
+++ frontend/src/components/AddMessage.vue
@@ -25,10 +25,13 @@ export default {
   methods: {
     async addMessage() {
       if (this.message) {
-        let { data } = await axios.post("/api/messages/create", {
-          userName: sessionStorage.getItem("userName"),
-          text: this.message,
-        });
+        let { data } = await axios.post(
+          "http://localhost:5000/messages/create",
+          {
+            userName: sessionStorage.getItem("userName"),
+            text: this.message,
+          }
+        );
         this.message = "";
         socket.emit("newMessage", data);
       }
