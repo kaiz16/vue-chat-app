1d6dc8d4b1cd165774819bb119ab79f4b5620a2a
mohammadsultani <mohammadssultani44@gmail.com>
2020-10-09 20:27:48 +0800

    Deploying

--- frontend/src/components/AddMessage.vue
+++ frontend/src/components/AddMessage.vue
@@ -14,7 +14,7 @@
 <script>
 /* eslint-disable */
 import io from "socket.io-client";
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect("https://thc-chat-app.herokuapp.com");
 import axios from "axios";
 export default {
   data() {
