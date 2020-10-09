aa07efdd19d58288884705f9573383f09835485b
mohammadsultani <mohammadssultani44@gmail.com>
2020-10-09 12:46:55 +0800

    Commiting

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,17 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
-import axios from "axios";
 Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
-const http = axios.create({
-  baseURL: process.env.VUE_APP_BACKEND_URL //http://localhost:3000/api => api
-});
-console.log(process.env)
 
-Vue.prototype.$http = http;
 
 new Vue({
   render: h => h(App),
